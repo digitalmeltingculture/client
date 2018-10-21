29b0833fb7e3692d0e5ffca5bf1489ac18844720
digitalmeltingculture <davidemariacalandra@gmail.com>
2018-10-21 13:46:34 +0200

    Davide: delineata struttura funzioni send

--- include/constants.h
+++ include/constants.h
@@ -11,8 +11,12 @@
 class Constants{
 	public:
 		const static int MAX_BUFFER_SIZE = 1024;
+		const static int SMALL_BUFFER_SIZE = 128;
+		const static int NUMERIC_BUFFER_SIZE = 8;
+
 		const static int DEFAULT_PORT_NUMBER = 8080;
 		const static int MAX_CONNECTIONS_NUMBER = 5;
+
 		const static int END_CONNECTION = 0;
 		const static int BYTE = 1;
 		const static int BOOLEAN = 2;
@@ -21,7 +25,11 @@ class Constants{
 		const static int FLOAT = 5;
 		const static int DOUBLE = 8;
 		const static int STRING = 9;
-		const static int BUFFER_DIMENSION = 8;
+
+		static constexpr const char* ERROR_MSG_SEND_TO_SERVER = "Error in sending data to server";
+
+		static constexpr const char* ERROR_MSG_SEND_INTEGER	  = "Error in sending integer data";
+		static constexpr const char* ERROR_MSG_SEND_STRING	  = "Error in sending string data";
 
 };
 
